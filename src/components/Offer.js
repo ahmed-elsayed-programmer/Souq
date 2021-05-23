eda9f7f2ee749853807f9b3252d49ca1afb8529d
ahmed elsayed <ahmedelsayed3157@gmail.com>
2021-05-23 15:33:27 +0200

    add resopnive

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -6,16 +6,16 @@ function Offer() {
             <div className="row">
                 <div className="col-lg-9 col-md-12">
                     <div className="row">
-                        <div className="col-3">
+                        <div className="col-md-3 col-6">
                             <img className='img-fluid' src="https://souqcms.s3.eu-west-1.amazonaws.com/sanfronto/eg/2020/web/Banners/top-leaderboard/May/LB-DOD-en.jpg" alt="" />
                         </div>
-                        <div className="col-3">
+                        <div className="col-md-3 col-6">
                             <img className='img-fluid' src="https://souqcms.s3.eu-west-1.amazonaws.com/sanfronto/eg/2020/web/Banners/top-leaderboard/May/LB-new-arrival-en.jpg" alt="" />
                         </div>
-                        <div className="col-3">
+                        <div className="col-md-3 col-6">
                             <img className='img-fluid' src="https://souqcms.s3-eu-west-1.amazonaws.com/sanfronto/eg/2021/Web/Top-Leaderboard/May/May2-NTA-en.jpg" alt="" />
                         </div>
-                        <div className="col-3">
+                        <div className="col-md-3 col-6">
                             <img className='img-fluid' src="https://souqcms.s3-eu-west-1.amazonaws.com/sanfronto/eg/2021/Web/Top-Leaderboard/May/AYHN-countdown-en.jpg" alt="" />
                         </div>
                     </div>
