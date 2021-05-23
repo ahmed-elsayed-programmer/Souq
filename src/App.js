7255e756a2c2a2b82cc378e96c1a852293ccbea0
ahmed elsayed <ahmedelsayed3157@gmail.com>
2021-05-23 15:40:45 +0200

    fix some issues

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header'
 
 const App = () => {
     return (
-        <div className="app bg-light">
+        <div className="app bg-light contanier">
             <Header />
             <Home />
         </div>
